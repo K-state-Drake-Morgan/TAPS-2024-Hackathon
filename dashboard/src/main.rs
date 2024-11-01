4a337a11a9d9b12277d0e49323fec2d7e11f02f3
K-state-Drake-Morgan <drakemorgan@ksu.edu>
2024-10-31 20:35:35 -0500

    maby working chart

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -1,14 +1,13 @@
-use std::io::Cursor;
+use std::{io::Cursor, sync::{Arc, Mutex}};
 
 // Import necessary libraries for plotting, web canvas, and Yew framework
 use plotters::prelude::*;
 use plotters_canvas::CanvasBackend;
 use reqwest;
-use wasm_bindgen_futures;
 use web_sys::HtmlCanvasElement;
 use yew::prelude::*;
 use gloo::console; // For logging messages to the browser console
-use calamine::{open_workbook_auto_from_rs, RangeDeserializerBuilder, Reader}; // parse xlsx files
+use calamine::{open_workbook_auto_from_rs, DataType, Reader}; // parse xlsx files
 
 // Enum to define the different plot messages that can trigger a plot update
 pub enum PlotMessage {
@@ -25,7 +24,8 @@ pub enum Message {
 
 // Main application structure containing a reference to the canvas
 pub struct App {
-    plot: NodeRef // NodeRef for accessing the canvas element
+    plot: NodeRef, // NodeRef for accessing the canvas element
+    plot_data: Vec<(f32, f32)>
 }
 
 // Implement the Component trait for the App struct
@@ -36,7 +36,8 @@ impl Component for App {
     // Function to create the App component instance
     fn create(_ctx: &Context<Self>) -> Self {
         App {
-            plot: NodeRef::default() // Initialize NodeRef for the canvas
+            plot: NodeRef::default(), // Initialize NodeRef for the canvas
+            plot_data: Vec::new()
         }
     }
 
@@ -50,7 +51,12 @@ impl Component for App {
                         // Log message to console for debugging
                         console::log!("Hello!");
 
-                        wasm_bindgen_futures::spawn_local(async { let _ = fetch_data().await; });
+                        let app = Arc::new(Mutex::new(App{plot: NodeRef::default(), plot_data: Vec::new()}));
+                        let app_f = Arc::clone(&app);
+
+                        wasm_bindgen_futures::spawn_local( async move {
+                            App::fetch_data(app_f.lock().unwrap()).await;
+                        });
 
                         // Get the canvas element from the NodeRef
                         let element: HtmlCanvasElement = self.plot.cast().unwrap();
@@ -67,62 +73,34 @@ impl Component for App {
                         // Create a drawing area for the plot
                         let drawing_area = backend.into_drawing_area();
                         drawing_area.fill(&RGBColor(200, 200, 200)).unwrap(); // Fill background with light gray
-                                    
+                        
+                        let min_x = app.lock().unwrap().plot_data.clone().into_iter().min_by_key(|x| x.0 as u64).unwrap().0;
+                        let max_x = app.lock().unwrap().plot_data.clone().into_iter().max_by_key(|x| x.0 as u64).unwrap().0;
+
+                        let min_y = app.lock().unwrap().plot_data.clone().into_iter().min_by_key(|x| x.1 as u64).unwrap().1;
+                        let max_y = app.lock().unwrap().plot_data.clone().into_iter().max_by_key(|x| x.1 as u64).unwrap().1;
+
                         // Build the chart with specific configurations
                         let mut chart = ChartBuilder::on(&drawing_area)
                             .caption("y=x^2", ("sans-serif", 14).into_font()) // Set title and font
                             .margin(5) // Set margins for the chart
                             .x_label_area_size(30) // Space for x-axis labels
                             .y_label_area_size(30) // Space for y-axis labels
-                            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).unwrap(); // Define the axis ranges
+                            .build_cartesian_2d(min_x..max_x, min_y..max_y).unwrap(); // Define the axis ranges
                         
                         // Configure and draw the mesh/grid of the chart
                         chart.configure_mesh().draw().unwrap();
                         
                         // Draw the series for y = x^2
                         chart.draw_series(LineSeries::new(
-                            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)), // Calculate points
+                            app.lock().unwrap().plot_data.clone(), // Calculate points
                             &BLUE, // Color of the line
                         )).unwrap()
                         .label("y = x^2") // Label for the legend
                         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED)); // Legend line element
 
                     },
-                    PlotMessage::ByeWorld => {
-                        // Log message to console for debugging
-                        console::log!("Bye!");
-
-                        // Similar logic as HelloWorld, but for ByeWorld
-                        let element: HtmlCanvasElement = self.plot.cast().unwrap();
-                        let parent = element.parent_element().unwrap();
-                        let rect = parent.get_bounding_client_rect();
-                        
-                        element.set_height(rect.height() as u32);
-                        element.set_width(rect.width() as u32);
-
-                        let backend = CanvasBackend::with_canvas_object(element).unwrap();
-                        
-                        let drawing_area = backend.into_drawing_area();
-                        drawing_area.fill(&RGBColor(200, 200, 200)).unwrap();
-                                    
-                        let mut chart = ChartBuilder::on(&drawing_area)
-                            .caption("y=x^2", ("sans-serif", 14).into_font())
-                            .margin(5)
-                            .x_label_area_size(30)
-                            .y_label_area_size(30)
-                            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).unwrap();
-                        
-                        chart.configure_mesh().draw().unwrap();
-                        
-                        // Draw the series for y = x^2 but with negative x-values
-                        chart.draw_series(LineSeries::new(
-                            (-50..=50).map(|x| -x as f32 / 50.0).map(|x| (x, x * x)), // Inverted x-values
-                            &RED, // Color of the line
-                        )).unwrap()
-                        .label("y = x^2") // Label for the legend
-                        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED)); // Legend line element
-
-                    },
+                    PlotMessage::ByeWorld => {},
                     PlotMessage::None => {}, // No action for None message
                 }
                 true // Indicate that the state has changed
@@ -163,31 +141,64 @@ impl Component for App {
     }
 }
 
-async fn fetch_data() -> Result<Vec<(String, f64)>, String> {
-    console::log!("Fetching DATA!");
-    let file = reqwest::get("http://127.0.0.1:3000/TAPS-2024-Hackathon/data/sensor_data/24 KSU TAPS AquaSpy.xlsx").await.unwrap();
-    let bytes = file.bytes().await.unwrap();
-    let bytes_wrapper = Cursor::new(bytes);
+impl App {
+    async fn fetch_data(mut state: std::sync::MutexGuard<'_, App>) {
+        console::log!("Fetching data...");
 
-    console::log!("Data Sharing to workbook");
-    let mut workbook = open_workbook_auto_from_rs(bytes_wrapper).map_err(|e| e.to_string())?;
-    let range = workbook.worksheet_range("Team #12 Data").expect("Sheet not found!");
-    
-    console::log!("Deserializing");
-    let deserializer = RangeDeserializerBuilder::new().from_range(&range).map_err(|e| e.to_string())?;
-    let mut data = Vec::new();
+        // Fetch the file
+        let response = reqwest::get("http://127.0.0.1:3000/TAPS-2024-Hackathon/data/sensor_data/24 KSU TAPS AquaSpy.xlsx")
+            .await
+            .map_err(|e| format!("Failed to fetch data: {}", e)).unwrap();
+            
+        let bytes = response.bytes()
+            .await
+            .map_err(|e| format!("Failed to read response bytes: {}", e)).unwrap();
+        
+        let bytes_wrapper = Cursor::new(bytes);
+        console::log!("Data shared to workbook...");
 
-    console::log!("Resulting!");
-    for result in deserializer {
-        let (name, value): (String, f64) = result.map_err(|e| e.to_string()).unwrap_or(("BAD".to_string(), 0.0));
-        data.push((name, value));
+        // Open the workbook
+        let mut workbook = open_workbook_auto_from_rs(bytes_wrapper)
+            .map_err(|e| format!("Failed to open workbook: {}", e)).unwrap();
+        
+        // Get the specified worksheet
+        let range = workbook.worksheet_range("Team #12 Data").unwrap();
+
+        console::log!("Deserializing data...");
+        let mut data = Vec::new();
+
+        // Iterate through rows
+        for row in range.rows() {
+            if row.len() > VALUE_COLUMN {
+                let name = row[NAME_COLUMN].clone();
+                let a: Option<f64> = match name {
+                    calamine::Data::Int(x) => Some(x as f64),
+                    calamine::Data::Float(x) => Some(x),
+                    calamine::Data::String(_x) => None,
+                    calamine::Data::Bool(_x) => None,
+                    calamine::Data::DateTime(excel_date_time) => Some(excel_date_time.as_f64()),
+                    calamine::Data::DateTimeIso(_x) => None,
+                    calamine::Data::DurationIso(_x) => None,
+                    calamine::Data::Error(_cell_error_type) => None,
+                    calamine::Data::Empty => None,
+                };
+                let keyn = a.unwrap_or(0.0) as f32;
+                let value = calamine::DataType::get_float(&row[VALUE_COLUMN]).unwrap_or(0.0) as f32;
+                data.push((keyn, value));
+            }
+        }
+
+        console::log!("Data retrieved successfully!");
+        console::log!(format!("{:?}", data));
+
+        state.plot_data = data;
     }
-
-    console::log!("Printing data!");
-    console::log!(format!("{:?}", data));
-
-    Ok(data)
 }
+
+// Assume X is the index for the name and Y is for the value
+const NAME_COLUMN: usize = 0 /* index for column X */;
+const VALUE_COLUMN: usize = 1 /* index for column Y */;
+
 
 // Entry point of the application
 fn main() {
